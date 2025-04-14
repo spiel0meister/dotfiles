a1796c03b547b449966112658977a27c2964a932
spiel0meister <soviczan7@gmail.com>
2025-04-14 02:06:16 +0200

    fixed bug

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,15 +4,15 @@ end
 function fish_prompt
     function get_git_branch
         if which git > /dev/null
-            echo $(git branch --show-current 2> /dev/null)
+            printf "%s" "$(git branch --show-current 2> /dev/null)"
         end
     end
 
     set branch $(get_git_branch)
-    if test -n branch
-        echo "[34m$USER[0m@[34m$hostname[0m [32m$PWD[0m ([33m$branch[0m)\$ "
-    else
+    if test -z $branch
         echo "[34m$USER[0m@[34m$hostname[0m [32m$PWD[0m\$ "
+    else
+        echo "[34m$USER[0m@[34m$hostname[0m [32m$PWD[0m ([33m$branch[0m)\$ "
     end
 end
 
