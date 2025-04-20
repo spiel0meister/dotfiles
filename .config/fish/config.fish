54194fff007c08d26c9fce0816e6582526f48e2e
spiel0meister <soviczan7@gmail.com>
2025-04-20 21:37:35 +0200

    added status in prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,12 @@ function fish_greeting
 end
 
 function fish_prompt
+    function get_git_status
+        if which git > /dev/null
+            printf "%s" "$(git status --short 2> /dev/null)"
+        end
+    end
+
     function get_git_branch
         if which git > /dev/null
             printf "%s" "$(git branch --show-current 2> /dev/null)"
@@ -9,11 +15,18 @@ function fish_prompt
     end
 
     set branch $(get_git_branch)
-    if test -z $branch
-        echo "[34m$USER[0m@[34m$hostname[0m [32m$PWD[0m\$ "
-    else
-        echo "[34m$USER[0m@[34m$hostname[0m [32m$PWD[0m ([33m$branch[0m)\$ "
+    set git_status $(get_git_status)
+
+    set prompt "[34m$USER[0m@[34m$hostname[0m [32m$PWD[0m"
+    if test -n $branch
+        set prompt $(string join "" $prompt " ([33m$branch[0m)")
     end
+    if test -n $status
+        set prompt $(string join "" $prompt "< ")
+    end
+    set prompt $(string join "" $prompt "\$ ")
+    
+    echo -n "$prompt"
 end
 
 alias ls="ls -al --color"
