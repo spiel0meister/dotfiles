76948af9761e3b627a4d4b81d089caa4245c6a8c
spiel0meister <soviczan7@gmail.com>
2025-04-20 21:51:08 +0200

    added status of being ahead

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,27 +2,22 @@ function fish_greeting
 end
 
 function fish_prompt
-    function get_git_status
-        if which git > /dev/null
-            echo -n "$(git status --short 2> /dev/null)"
-        end
-    end
-
-    function get_git_branch
-        if which git > /dev/null
-            echo -n "$(git branch --show-current 2> /dev/null)"
-        end
-    end
-
-    set branch $(get_git_branch)
-    set git_status $(get_git_status)
-
     set prompt "[34m$USER[0m@[34m$hostname[0m [32m$PWD[0m"
-    if test -n "$branch"
-        set prompt $(string join "" $prompt " ([33m$branch[0m)")
-    end
-    if test -n "$git_status"
-        set prompt $(string join "" $prompt "< ")
+    if which git > /dev/null
+        if git status > /dev/null 2> /dev/null
+            set branch "$(git branch --show-current)"
+            set prompt $(string join "" $prompt " ([33m$branch[0m)")
+
+            if test -n "$(git status -b --short 2> /dev/null)"
+                set prompt $(string join "" $prompt "^")
+            end
+
+            if test -n "$(git status --short 2> /dev/null)"
+                set prompt $(string join "" $prompt "<")
+            end
+
+            set prompt $(string join "" $prompt " ")
+        end
     end
     set prompt $(string join "" $prompt "\$ ")
     
